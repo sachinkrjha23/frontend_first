c1a986f3e0f5f78492dd14765301ccb9240afffe
Sachin Kumar Jha <skrjha002@gmail.com>
2026-03-28 15:15:16 +0530

    feat: build Instamart-style grocery section with dynamic rendering �

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
+import FoodOption from "./Components/FoodOption"
+import GroceryOption from "./Components/GroceryOption";
 
 
 function App(){
     return(
         <>
             <Header></Header>
+            <FoodOption></FoodOption>
+            <GroceryOption></GroceryOption>
         </>
     )
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,10 +3,9 @@
 export default function Header(){
     return (
         <header className="bg-[#ff5200] font-serif">
-            {/* first div: Swiggy Logo, Sign in button, Get App, etc */}
-            <div className="flex justify-between container mx-auto py-8 px-8">
+            <div className="max-w-300 mx-auto px-4 py-8 flex justify-between items-center">
                 <img className="w-40 h-12" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png"></img>
-                <div className="text-white text-base font-bold flex gap-8 items-center">
+                <div className="text-white text-base font-bold flex  gap-15 items-center">
                     <a target="_blank" href="https://www.swiggy.com/corporate/">Swiggy Corporate</a>
                     <a target="_blank" href="https://partner.swiggy.com/food/login">Partner with us</a>
                     <a className="border border-white py-3 px-4 rounded-2xl" target="_blank" href="">Get the App</a>
@@ -20,13 +19,23 @@ export default function Header(){
                 <div className="text-5xl text-white font-bold max-w-[60%] container mx-auto text-center">
                     Order food & groceries. Discover best restaurants. Swiggy it!
                 </div>
-                <div className="max-w-[70%] container mx-auto flex gap-5 mt-10">
-                    <input className="bg-white w-[40%] text-2xl px-6 py-4 rounded-2xl" placeholder="Delhi, India"></input> 
-                    <input className="bg-white w-[55%] text-2xl px-6 py-4 rounded-2xl" placeholder="Search for restaurant, item or more"></input> 
+                <div className="max-w-[60%] container mx-auto flex justify-center gap-5 mt-10">
+                    <input className="bg-white w-[35%] text-xl px-6 py-4 rounded-2xl " placeholder="Delhi, India"></input> 
+                    <input className="bg-white w-[45%] text-xl px-6 py-4 rounded-2xl" placeholder="Search for restaurant, item or more"></input> 
                 </div>
             </div>
 
-            <div></div>
+            <div className="max-w-[80%] container mx-auto flex">
+                <a href="https://www.swiggy.com/restaurants">
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/ec86a309-9b06-48e2-9adc-35753f06bc0a_Food3BU.png"></img>
+                </a>
+                <a href="https://www.swiggy.com/instamart?entryId=1234&entryName=mainTileEntry4&v=1">
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b5c57bbf-df54-4dad-95d1-62e3a7a8424d_IM3BU.png"></img>
+                </a>
+                <a href="https://www.swiggy.com/dineout">
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/23/b6d9b7ab-91c7-4f72-9bf2-fcd4ceec3537_DO3BU.png"></img>
+                </a>
+            </div>
         </header>
 
     )
