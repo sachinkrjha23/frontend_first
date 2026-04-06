d51b31a751fa43dea46f850ae81175247e92cf32
Sachin Kumar Jha <skrjha002@gmail.com>
2026-04-06 09:58:43 +0530

    Fix for  deployment

--- src/Components/GroceryOption.js
+++ src/Components/GroceryOption.js
@@ -1,5 +1,5 @@
 import {GrocerGridCard} from "../Utils/Grocery"
-import Grocerycard from "./GroceryCard"
+import GroceryCard from "./GroceryCard"
 
 export default function GroceryOption(){
 
@@ -9,7 +9,7 @@ export default function GroceryOption(){
             <h1 className="text-2xl font-bold">Shop Groceries on Instamart</h1>
             <div className="container mx-auto flex flex-nowrap overflow-x-auto mt-5 gap-3">
                     {
-                        GrocerGridCard.map((foodData)=><Grocerycard key={foodData.id} foodData={foodData}></Grocerycard>)
+                        GrocerGridCard.map((foodData)=><GroceryCard key={foodData.id} foodData={foodData}></GroceryCard>)
                     }
                   </div>
         </div>
